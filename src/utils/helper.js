0d70a16d105d84d1b090b6c79410c49a9a850485
ethon92 <e19920526@gmail.com>
2022-03-01 15:57:17 +0800

    change api link

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,8 +3,7 @@ import Swal from "sweetalert2";
 import axios from "axios";
 
 export const apiHelper = axios.create({
-  // baseURL: "http://192.168.31.244:3000/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://192.168.31.244:3000/api",
 });
 
 apiHelper.interceptors.request.use(
