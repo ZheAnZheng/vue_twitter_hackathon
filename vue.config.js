22a0fc6a52bb7beba04b687b153fa62537864d3f
Nixon <a37007647@gmail.com>
2022-03-04 11:53:59 +0000

    feat:add Public chat room view

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue_twitter/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue_twitter_hackathon/" : "/",
 };
